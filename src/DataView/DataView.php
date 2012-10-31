e96175960be405a5ebe883b3aae1892a33b6129a
Martin Parsiegla <parsiegla@kuponjo.de>
2012-10-31 15:31:39 +0100

    [master] Added phpunit boostraping and a test for the DataView class.

--- src/DataView/DataView.php
+++ src/DataView/DataView.php
@@ -3,101 +3,110 @@
 namespace DataView;
 
 use DataView\Adapter\AdapterInterface;
+use Pagerfanta\Pagerfanta;
 
 /**
  * Given a source, this class can apply a set of filters and sort it into a paginated list.
  *
  * @package DataView
- * @author George Zankevich <gzankevich@gmail.com> 
+ * @author George Zankevich <gzankevich@gmail.com>
  */
 class DataView
 {
-	const SORT_ORDER_ASCENDING = 'ascending';
-	const SORT_ORDER_DESCENDING = 'descending';
+    const SORT_ORDER_ASCENDING = 'ascending';
+    const SORT_ORDER_DESCENDING = 'descending';
 
-	private $orderByPropertyPath, $sortOrder = null;
-	private $filters, $columns = array();
+    private $orderByPropertyPath = null;
+    private $sortOrder = null;
+    private $filters = array();
+    private $columns = array();
 
-	/**
-	 * Constructor
-	 *
-	 * The ORM/ODM adapter is injected here.
-	 *
-	 * @param AdapterInterface $adapter The adapter to use (e.g. DoctrineORM)
-	 * @return null
-	 */
-	public function __construct(AdapterInterface $adapter)
-	{
-		$this->adapter = $adapter;
-	}
+    /**
+     * Constructor
+     *
+     * The ORM/ODM adapter is injected here.
+     *
+     * @param AdapterInterface $adapter The adapter to use (e.g. DoctrineORM)
+     */
+    public function __construct(AdapterInterface $adapter)
+    {
+        $this->adapter = $adapter;
+    }
 
-	/**
-	 * Can be a query builder, entity name, array (depends on what the adapter supports)
-	 *
-	 * @param mixed $source The source
-	 * @return null
-	 */
-	public function setSource($source)
-	{
-		$this->adapter->setSource($source);
-	}
+    /**
+     * Can be a query builder, entity name, array (depends on what the adapter supports)
+     *
+     * @param mixed $source The source
+     *
+     * @return null
+     */
+    public function setSource($source)
+    {
+        $this->adapter->setSource($source);
+    }
 
-	/**
-	 * Add a filter
-	 *
-	 * @param Filter $filter The filter to add
-	 * @return null
-	 */
-	public function addFilter(Filter $filter)
-	{
-		$this->filters[] = $filter;
-	}
+    /**
+     * Add a filter
+     *
+     * @param Filter $filter The filter to add
+     *
+     * @return null
+     */
+    public function addFilter(Filter $filter)
+    {
+        $this->filters[] = $filter;
+    }
 
-	/**
-	 * Assign a set of filters
-	 *
-	 * @param array $filters The filters to set
-	 * @return null
-	 */
-	public function setFilters($filters)
-	{
-		$this->filters = $filters;
-	}
+    /**
+     * Assign a set of filters
+     *
+     * @param array $filters The filters to set
+     *
+     * @return null
+     */
+    public function setFilters($filters)
+    {
+        $this->filters = $filters;
+    }
 
-	/**
-	 * Add a filter
-	 *
-	 * @param Column $column The column to add
-	 * @return null
-	 */
-	public function addColumn(Column $column)
-	{
-		$this->columns[] = $column;
-	}
+    /**
+     * Add a filter
+     *
+     * @param Column $column The column to add
+     *
+     * @return null
+     */
+    public function addColumn(Column $column)
+    {
+        $this->columns[] = $column;
+    }
 
-	/**
-	 * Assign a set of filters
-	 *
-	 * @param array $columns The columns to set
-	 * @return null
-	 */
-	public function setColumns($columns)
-	{
-		$this->columns = $columns;
-	}
+    /**
+     * Assign a set of filters
+     *
+     * @param array $columns The columns to set
+     *
+     * @return null
+     */
+    public function setColumns($columns)
+    {
+        $this->columns = $columns;
+    }
 
-	/**
-	 * Set the order by column and the sort order
-	 *
-	 * @param string $propertyPath The property path of the column to order by
-	 * @param string $sortOrder Ascending or descending
-	 * @return null
-	 */
-	public function setOrderBy($propertyPath, $sortOrder)
-	{
-		$this->orderByPropertyPath = $propertyPath;
-		$this->sortOrder = $sortOrder;
-	}
+    /**
+     * Set the order by column and the sort order
+     *
+     * @param string $propertyPath The property path of the column to order by
+     * @param string $sortOrder Ascending or descending
+     *
+     * @return null
+     */
+    public function setOrderBy($propertyPath, $sortOrder)
+    {
+        $this->orderByPropertyPath = $propertyPath;
+        $this->sortOrder = $sortOrder;
+    }
+
 
 	/**
 	 * Gets the results from the adapter in a pager
@@ -110,11 +119,9 @@ class DataView
 			throw new SourceNotSetException('Please set a source to fetch the results from');
 		}
 
-		$this->adapter->setFilters($this->filters);
-		$this->adapter->setOrderBy($this->orderByPropertyPath, $this->sortOrder);
+        $this->adapter->setFilters($this->filters);
+        $this->adapter->setOrderBy($this->orderByPropertyPath, $this->sortOrder);
 
-		return $this->adapter->getPager(
-			$this->adapter->getQuery()
-		);
-	}
+        return $this->adapter->getPager();
+    }
 }
